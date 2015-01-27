9c3c70944eb9f20875784ba2db205d5f160149aa
unknown <BDonnot@PC48002.ensae.fr>
2015-01-27 17:10:41 +0100

    adding the TO DO

--- FirstTest.R
+++ FirstTest.R
@@ -9,3 +9,16 @@ maxBreakpoints <- 10
 classic = cghseg:::segmeanCO(signal, maxBreakpoints)
 pruned = jointSeg:::pruneByDP(as.matrix(signal,nrow = 1), K=maxBreakpoints)
 pruned$rse
+
+#TO DO :
+##Comparer les deux algos, avec temps de calcul et precision
+#des estimations -> donnees artificielles
+#-> verifier que la solution est la meme
+##faire un truc avec des vraies donnees
+
+#Rapport :
+##presenter la problematique
+##presenter l'algo classique
+##donner l'intuition de l'algo speed-up 'ed
+##montrer les resultats
+##conclure
