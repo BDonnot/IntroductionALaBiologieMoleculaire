4d4371e6230630c55654afc30d024bc68278ac31
Benjamin DONNOT <benjamin.donnot@gmail.com>
2015-01-30 16:57:51 +0100

    aading the plots

--- graphs.R
+++ graphs.R
@@ -1,7 +1,8 @@
-install.packages("cghseg")
-source("http://bioconductor.org/biocLite.R")
-biocLite("DNAcopy")
-install.packages("jointseg",repos="http://R-Forge.R-project.org")
+if(F){install.packages("cghseg")
+  source("http://bioconductor.org/biocLite.R")
+  biocLite("DNAcopy")
+  install.packages("jointseg",repos="http://R-Forge.R-project.org")
+}
 library("cghseg")
 library("jointseg")
 library("rbenchmark")
@@ -9,22 +10,45 @@ library("rbenchmark")
 
 
 set.seed(42)
-allSizes = c(10,100,200,300,500,1000)
-res = rep(0,length(allSizes))
+allSizes = c(10,100,200,300,500,1000,2000,5000,10000)
+resSize = rep(0,length(allSizes))
 i = 1
 for (size in allSizes){
-sizeEach = c(350,250,50,350)*size
-sigma = .3
-gamma = rep(c(2,1,2,3), sizeEach)
-
-c = gamma + rnorm(length(gamma),0,sigma)
-# matC= as.matrix(c,ncol = 1)
-maxBreakpoints = 3
-# classic = cghseg:::segmeanCO(c, maxBreakpoints+1)
-pruned = doDynamicProgramming(c, K=maxBreakpoints)
-res[i] = system.time(doDynamicProgramming(c, K=maxBreakpoints))["user.self"]
-i = i+1
+  sizeEach = c(350,250,50,350)*size
+  sigma = .3
+  gamma = rep(c(2,1,2,3), sizeEach)
+  c = gamma + rnorm(length(gamma),0,sigma)
+  maxBreakpoints = 3
+  resSize[i] = system.time(doDynamicProgramming(c, K=maxBreakpoints))["user.self"]
+  i = i+1
 }
+allSizesT = allSizes*1000
+plot(allSizesT,resSize,pch = "+"
+     ,ylab = "time"
+     , xlab= "sample size (J)"
+     ,main = "Time complexity with respect to J")
+abline(lm(resSize~allSizesT),col = "blue")
+#3 breaks
+
+allK = c(5,10,20,30,50,75,100)
+resK = rep(0,length(allK))
+sizeEach = c(350,250,50,350)*100
+sigma = .3
+i = 1
+for (K in allK){
+    gamma = rep(rep(c(2,1,2,3),K/5), rep(round(sizeEach/(K/5),0),K/5) )
+    c = gamma + rnorm(length(gamma),0,sigma)
+    maxBreakpoints = K
+    resK[i] = system.time(doDynamicProgramming(c, K=maxBreakpoints))["user.self"]
+    i = i+1
+}
+plot(allK,resK,pch = "+"
+     ,ylab = "time"
+     , xlab= "number of breaks"
+     ,main = "Time complexity with respect to K")
+abline(lm(resK~allK),col = "blue")
+#1e5 points
+
 # bestPruned = Segmentor(c,keep=T,model = 2,Kmax = maxBreakpoints+1)
 if(F){
   plot(c)
